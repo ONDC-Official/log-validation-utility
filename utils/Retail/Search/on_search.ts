179a77f781bbab0aa78937ba7de4624964ef630f
kavyagarg05 <kavya3552.be20@chitkara.edu.in>
2024-02-19 16:55:00 +0530

    fix/upcoming holidays on_search

--- utils/Retail/Search/on_search.ts
+++ utils/Retail/Search/on_search.ts
@@ -232,6 +232,7 @@ export const checkOnsearch = (data: any, msgIdSet: any) => {
       })
 
       try {
+        logger.info(`Checking for upcoming holidays`)
         const location = onSearchCatalog['bpp/providers'][i]['locations']
         if (!location) {
           logger.error('No location detected ')
@@ -244,7 +245,7 @@ export const checkOnsearch = (data: any, msgIdSet: any) => {
         scheduleObject.map((date: string) => {
           const dateObj = new Date(date)
           const currentDateObj = new Date(currentDate)
-          if (dateObj.getTime() > currentDateObj.getTime()) {
+          if (dateObj.getTime() < currentDateObj.getTime()) {
             const key = `/message/catalog/bpp/providers/loc${i}/time/schedule/holidays`
             errorObj[key] = `Holidays cannot be past ${currentDate}`
           }

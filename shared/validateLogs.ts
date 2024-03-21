6e12ff8102089feadcd8a57e7fecbe717705f367
extedcouD <89811591+extedcouD@users.noreply.github.com>
2024-03-21 17:55:28 +0530

    RSF

--- shared/validateLogs.ts
+++ shared/validateLogs.ts
@@ -2,7 +2,7 @@ import _ from 'lodash'
 import { checkSearchFullCatalogRefresh } from '../utils/Retail/RET11/searchFullCatalogRefresh'
 import { dropDB, setValue } from '../shared/dao'
 import { logger } from './logger'
-import { ApiSequence, retailDomains, IGMApiSequence } from '../constants'
+import { ApiSequence, retailDomains, IGMApiSequence, RSFapiSequence } from '../constants'
 import { validateSchema, isObjectEmpty } from '../utils'
 import { checkOnsearchFullCatalogRefresh } from '../utils/Retail/RET11/onSearch'
 import { checkSelect } from '../utils/Retail/RET11/select'
@@ -41,6 +41,8 @@ import { checkOnStatusOutForDelivery } from '../utils/Retail/Status/onStatusOutF
 import { checkOnStatusDelivered } from '../utils/Retail/Status/onStatusDelivered'
 import { checkCancel } from '../utils/Retail/Cancel/cancel'
 import { checkOnCancel } from '../utils/Retail/Cancel/onCancel'
+import checkRsfReceiverRecon from '../utils/RSF/rsfReceiverRecon'
+import checkRsfOnReceiverRecon from '../utils/RSF/rsfOnReciverRecon'
 
 export const validateLogs = async (data: any, domain: string, flow: string) => {
   const msgIdSet = new Set()
@@ -392,6 +394,39 @@ export const IGMvalidateLogs = (data: any) => {
         logReport = { ...logReport, [IGMApiSequence.LSP_ON_ISSUE_STATUS]: lsp_on_issue }
       }
     }
+
+    logger.info(logReport, 'Report Generated Successfully!!')
+    return logReport
+  } catch (error: any) {
+    logger.error(error.message)
+    return error.message
+  }
+}
+
+export const RSFvalidateLogs = (data: any) => {
+  let logReport: any = {}
+
+  try {
+    dropDB()
+  } catch (error) {
+    logger.error('!!Error while removing LMDB', error)
+  }
+
+  try {
+    if (data[RSFapiSequence.RECEIVER_RECON]) {
+      const receiver_recon = checkRsfReceiverRecon(data[RSFapiSequence.RECEIVER_RECON])
+      if (!_.isEmpty(receiver_recon)) {
+        logReport = { ...logReport, [RSFapiSequence.RECEIVER_RECON]: receiver_recon }
+      }
+    }
+    if (data[RSFapiSequence.ON_RECEIVER_RECON]) {
+      const on_receiver_recon = checkRsfOnReceiverRecon(data[RSFapiSequence.ON_RECEIVER_RECON])
+
+      if (!_.isEmpty(on_receiver_recon)) {
+        logReport = { ...logReport, [RSFapiSequence.ON_RECEIVER_RECON]: on_receiver_recon }
+      }
+    }
+
     logger.info(logReport, 'Report Generated Successfully!!')
     return logReport
   } catch (error: any) {

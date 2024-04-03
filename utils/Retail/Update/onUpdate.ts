03208b52e0134f316480d9ccb0a0326308cc0067
Jagannath-Padhy <jagannathpadhy1999@gmail.com>
2024-04-03 06:56:27 +0530

    false error on_update timestamp

--- utils/Retail/Update/onUpdate.ts
+++ utils/Retail/Update/onUpdate.ts
@@ -72,17 +72,19 @@ export const checkOnUpdate = (data: any) => {
     }
 
     // Comaring Timestamp of /update with /init API
-    try {
-      logger.info(`Comparing timestamp of /${constants.ON_INIT} and /${constants.ON_UPDATE}`)
-      if (_.gte(getValue('tmpstmp'), context.timestamp)) {
-        onupdtObj.tmpstmp = `Timestamp for /${constants.ON_INIT} api cannot be greater than or equal to /${constants.ON_UPDATE} api`
-      }
+    if (flow !== '6-a'){
+      try {
+        logger.info(`Comparing timestamp of /${constants.ON_INIT} and /${constants.ON_UPDATE}`)
+        if (_.gte(getValue('tmpstmp'), context.timestamp)) {
+          onupdtObj.tmpstmp = `Timestamp for /${constants.ON_INIT} api cannot be greater than or equal to /${constants.ON_UPDATE} api`
+        }
 
-      setValue('tmpstmp', context.timestamp)
-    } catch (error: any) {
-      logger.error(
-        `!!Error while comparing timestamp for /${constants.ON_INIT} and /${constants.ON_UPDATE} api, ${error.stack}`,
-      )
+        setValue('tmpstmp', context.timestamp)
+      } catch (error: any) {
+        logger.error(
+          `!!Error while comparing timestamp for /${constants.ON_INIT} and /${constants.ON_UPDATE} api, ${error.stack}`,
+        )
+      }
     }
 
     // Comparing transaction ID with /select API

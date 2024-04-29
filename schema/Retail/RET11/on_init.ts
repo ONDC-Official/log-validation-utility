7e237f9f58cea362952210274abb7ace3fcddba3
Jagannath-Padhy <jagannathpadhy1999@gmail.com>
2024-04-29 11:55:46 +0530

    added gstin to tax_number validation schema

--- schema/Retail/RET11/on_init.ts
+++ schema/Retail/RET11/on_init.ts
@@ -649,7 +649,7 @@ export const FnBonInitSchema = {
                             properties: {
                               value: {
                                 type: 'string',
-                                pattern: '^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$',
+                                pattern: '^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$|^GSTIN[0-9]{10}$',
                                 errorMessage: 'Value for tax_number must be a valid tax number i.e alphanumeric with 15 characters ',
                               },
                             },

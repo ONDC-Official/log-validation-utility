dd473a6125bee978ad6909d83ef6200029e4c50a
Rupal Singla <rupalsingla@ONDC-DEL-M015.local>
2025-02-18 13:03:41 +0530

    changes

--- controller/validate/index.ts
+++ controller/validate/index.ts
@@ -56,7 +56,7 @@ const controller = {
           const { response, success, message } = await helper.validateIGM(payload, version)
           result = { response, success, message }
           break
-          case DOMAIN.RSF:
+        case DOMAIN.RSF:
           {
             const { response, success, message } = await helper.validateRSF(payload, version)
             result = { response, success, message }
--- utils/igm/igm2/on_issue_2.ts
+++ utils/igm/igm2/on_issue_2.ts
@@ -1,37 +1,8 @@
-// import _ from 'lodash'
-// import { setValue, getValue } from '../../../shared/dao'
-// import { checkContext, isObjectEmpty } from '../../../utils/index'
-// import constants, { IGMApiSequence } from '../../../constants/index'
-// import { validateSchema } from '../../../utils/index'
-// import { logger } from '../../../shared/logger'
-// import {
-//   checkCreatedAtInAll,
-//   checkDomainInAll,
-//   checkOrganizationNameandDomain,
-//   compareContextTimeStampAndUpdatedAt,
-//   compareUpdatedAtAndContextTimeStamp,
-// } from '.././igmHelpers'
-
-// const checkOnIssueV2 = (data: any,apiSequenceName: string) => {
-//   const onissueObj: any = {}
-//   let res: any = {}
-    
-//     if(apiSequenceName === "ON_ISSUE_1"){
-
-//     }
-//     if(apiSequenceName === "ON_ISSUE_2"){
-
-//     }
-// }
-
-// export default checkOnIssueV2
-
-
 import _ from 'lodash'
 import { setValue, getValue } from '../../../shared/dao'
-import { checkContext, isObjectEmpty } from '../../../utils/index'
+import { checkContext, isObjectEmpty } from '../../index'
 import constants, { IGMApiSequence } from '../../../constants/index'
-import { validateSchema } from '../../../utils/index'
+import { validateSchema } from '../../index'
 import { logger } from '../../../shared/logger'
 import {
   checkCreatedAtInAll,
@@ -41,9 +12,10 @@ import {
   compareUpdatedAtAndContextTimeStamp,
 } from '../igmHelpers'
 
-const checkOnIssue = (data: any,type: string) => {
+const checkOnIssue = (data: any, type: string) => {
   const onissueObj: any = {}
   let res: any = {}
+
   try {
     const on_issue: any = data
 
@@ -51,71 +23,69 @@ const checkOnIssue = (data: any,type: string) => {
       return { [IGMApiSequence.RET_ON_ISSUE]: 'JSON cannot be empty' }
     }
 
+    // Schema validation
     try {
       logger.info(`Validating Schema for ${constants.RET_ONISSUE} API`)
       const vs = validateSchema('igm', constants.RET_ONISSUE, on_issue)
-      if (vs != 'error') {
+      if (vs !== 'error') {
         Object.assign(onissueObj, vs)
       }
     } catch (error: any) {
-      logger.error(`!!Error occurred while performing schema validation for /${constants.RET_ONISSUE}, ${error.stack}`)
+      logger.error(`Error occurred while performing schema validation for /${constants.RET_ONISSUE}, ${error.stack}`)
     }
 
-    logger.info(`Checking context for ${constants.RET_ONISSUE} API`) //checking context
+    // Context validation
+    logger.info(`Checking context for ${constants.RET_ONISSUE} API`)
     try {
       res = checkContext(on_issue.context, constants.RET_ONISSUE)
       if (!res.valid) {
         Object.assign(onissueObj, res.ERRORS)
       }
     } catch (error: any) {
-      logger.error(`Some error occurred while checking /${constants.RET_ONISSUE} context, ${error.stack}`)
+      logger.error(`Error occurred while checking /${constants.RET_ONISSUE} context, ${error.stack}`)
     }
 
+    // Transaction ID validation
     try {
       logger.info(`Comparing transaction ID of /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`)
       if (!_.isEqual(getValue('igmTxnId'), on_issue.context.transaction_id)) {
-        onissueObj.igmTxnId = `transaction  ID mismatch in /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`
+        onissueObj.igmTxnId = `Transaction ID mismatch in /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`
       }
     } catch (error: any) {
-      logger.error(
-        `Error while comparing transaction ID in /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}, ${error.stack}`,
-      )
+      logger.error(`Error comparing transaction ID for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}, ${error.stack}`)
     }
 
+    // Message ID validation
     try {
       logger.info(`Comparing MESSAGE ID of /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`)
       if (!_.isEqual(getValue('igmIssueMsgId'), on_issue.context.message_id)) {
-        onissueObj.igmIssueMsgId = `Message  ID mismatch in /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`
+        onissueObj.igmIssueMsgId = `Message ID mismatch in /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`
       }
     } catch (error: any) {
-      logger.error(
-        `Error while comparing Message ID in /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}, ${error.stack}`,
-      )
+      logger.error(`Error comparing Message ID for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}, ${error.stack}`)
     }
 
+    // Domain validation
     try {
       logger.info(`Comparing Domain of /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`)
       if (!_.isEqual(getValue('igmDomain'), on_issue.context.domain)) {
-        onissueObj.igmDomain = `Domain for /${constants.RET_ISSUE} api should be equal to /${constants.RET_ONISSUE} api`
+        onissueObj.igmDomain = `Domain mismatch for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`
       }
     } catch (error: any) {
-      logger.error(
-        `Error while comparing Domain for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE} api, ${error.stack}`,
-      )
+      logger.error(`Error comparing Domain for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}, ${error.stack}`)
     }
 
+    // Core version validation
     try {
       logger.info(`Comparing core version of /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}`)
       setValue('core_version', on_issue.context.core_version)
     } catch (error: any) {
-      logger.error(
-        `Error while comparing core version for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE} api, ${error.stack}`,
-      )
+      logger.error(`Error comparing core version for /${constants.RET_ISSUE} and /${constants.RET_ONISSUE}, ${error.stack}`)
     }
 
+    // Phone number validation
     try {
       logger.info(`Phone Number Check for /${constants.RET_ONISSUE}`)
-
       if (
         !_.inRange(
           on_issue?.message?.issue?.issue_actions?.respondent_actions?.[0]?.updated_by?.contact?.phone,
@@ -123,26 +93,25 @@ const checkOnIssue = (data: any,type: string) => {
           99999999999,
         )
       ) {
-        onissueObj.Phn = `Phone Number for /${constants.RET_ONISSUE} api is not in the valid Range`
+        onissueObj.Phn = `Phone Number for /${constants.RET_ONISSUE} API is not in the valid range`
       }
     } catch (error: any) {
-      logger.error(`Error while checking phone number for /${constants.RET_ONISSUE} api, ${error.stack}`)
+      logger.error(`Error checking phone number for /${constants.RET_ONISSUE} API, ${error.stack}`)
     }
 
-    // try {
-    //   logger.info(
-    //     `Checking time of creation and updation for /${constants.RET_ONISSUE}`
-    //   );
-    //   if (!_.lte(issue.context.timestamp, on_issue.message.issue.created_at)) {
-    //     onissueObj.updatedTime = `Time of Creation for /${constants.RET_ONISSUE} api should be less than context timestamp`;
-    //   }
-    // } catch (error:any) {
-    //   logger.error(
-    //     `Error while checking time of creation and updation for /${constants.RET_ONISSUE} api, ${error.stack}`
-    //   );
-    // }
+    // Created/Updated Time validation
+    try {
+      logger.info(`Checking time of creation and updation for /${constants.RET_ONISSUE}`)
+      if (!_.lte(on_issue.context.timestamp, on_issue.message.issue.created_at)) {
+        onissueObj.updatedTime = `Time of Creation for /${constants.RET_ONISSUE} API should be less than context timestamp`
+      }
+    } catch (error: any) {
+      logger.error(`Error comparing time of creation and updation for /${constants.RET_ONISSUE} API, ${error.stack}`)
+    }
+
     setValue('igmCreatedAt', on_issue.message.issue.created_at)
 
+    // Action validations
     const respondent_actions = on_issue.message.issue.issue_actions.respondent_actions
 
     checkOrganizationNameandDomain({
@@ -167,7 +136,11 @@ const checkOnIssue = (data: any,type: string) => {
       issueReportObj: onissueObj,
     })
 
-    checkDomainInAll({ endpoint: constants.RET_ONISSUE, domain: on_issue.context.domain, issueReportObj: onissueObj })
+    checkDomainInAll({
+      endpoint: constants.RET_ONISSUE,
+      domain: on_issue.context.domain,
+      issueReportObj: onissueObj,
+    })
 
     compareContextTimeStampAndUpdatedAt({
       endpoint: constants.RET_ONISSUE,
@@ -178,16 +151,14 @@ const checkOnIssue = (data: any,type: string) => {
 
     setValue('onissueObj', onissueObj)
 
-    // setValue("onissueObj", onissueObj);
     return onissueObj
   } catch (err: any) {
     if (err.code === 'ENOENT') {
-      logger.info(`!!File not found for /${constants.RET_ONISSUE} API!`)
+      logger.info(`File not found for /${constants.RET_ONISSUE} API!`)
     } else {
-      logger.error(`!!Some error occurred while checking /${constants.RET_ONISSUE} API`, err)
+      logger.error(`Error occurred while checking /${constants.RET_ONISSUE} API`, err)
     }
   }
 }
 
 export default checkOnIssue
-

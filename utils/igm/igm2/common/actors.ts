ee7a87598bca3575746d95f08087eaa76349e619
amitsinha07 <aamitsinha456@gmail.com>
2025-03-11 00:28:53 +0530

    feat: added flow2 and flow3 for IGM 2.0.0

--- utils/igm/igm2/common/actors.ts
+++ utils/igm/igm2/common/actors.ts
@@ -1,9 +1,10 @@
 import _ from 'lodash'
 import { logger } from '../../../../shared/logger'
+import { IGM2Flows } from '../../../../utils/constants/index'
 
-export const validateActors = (actors: any[], flow: any) => {
+
+export const validateActors = (actors: any[], context: any, flow: any) => {
   const errors: any = {}
-  console.log("flow", flow)
   try {
     logger.info('Validating actors array')
 
@@ -12,8 +13,30 @@ export const validateActors = (actors: any[], flow: any) => {
     }
 
     actors.forEach((actor, index) => {
+      // Check actor type
+      if (!actor.type) {
+        errors[`actor_${index}_type`] = 'actor type is required'
+      }
       
-
+      // Check actor info
+      if (!actor.info) {
+        errors[`actor_${index}_info`] = 'actor info is required'
+      } else {
+        // Check org
+        if (!actor.info.org) {
+          errors[`actor_${index}_info_org`] = 'actor info.org is required'
+        } else if (!actor.info.org.name) {
+          errors[`actor_${index}_info_org_name`] = 'actor info.org.name is required'
+        } else if (flow === IGM2Flows.FLOW_1) {
+          const expectedFormat = getExpectedOrgNameFormat(actor.type, context);
+          
+          if (expectedFormat && actor.info.org.name !== expectedFormat) {
+            errors[`actor_${index}_info_org_name`] = 
+              `For actor type "${actor.type}", org.name should be "${expectedFormat}" (context.bap_id::context.domain)`
+          }
+        }
+      }
+      
       if (actor.info) {
         
         if (actor.info.contact) {
@@ -32,5 +55,25 @@ export const validateActors = (actors: any[], flow: any) => {
   } catch (error: any) {
     logger.error(`Error while validating actors: ${error.stack}`)
     return { actors_error: error.message }
+  }
+}
+
+// Helper function to determine the expected org.name format based on actor type
+function getExpectedOrgNameFormat(actorType: string, context: any): string | null {
+  if (!context || !context.domain) {
+    return null;
+  }
+  
+  switch (actorType) {
+    case 'CONSUMER':
+    case 'INTERFACING_NP':
+      return context.bap_id ? `${context.bap_id}::${context.domain}` : null;
+      
+    case 'COUNTERPARTY_NP':
+    case 'COUNTERPARTY_NP_GRO':
+      return context.bpp_id ? `${context.bpp_id}::${context.domain}` : null;
+      
+    default:
+      return null;
   }
 }
---
+++ utils/igm/igm2/constants.ts
@@ -0,0 +1,6 @@
+export const IGM2Flows = {
+  FLOW_1: 'FLOW_1',
+  FLOW_2: 'FLOW_2',
+  FLOW_3: 'FLOW_3',
+  FLOW_4: 'FLOW_4'
+} 

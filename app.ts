f22f7c61bfbfbe3a53b1461bd3854f0eafab7993
Nikita <nikita@WILASSETA0109.local>
2025-05-27 15:09:15 +0530

    feat: added ui folder

--- app.ts
+++ app.ts
@@ -1,19 +1,40 @@
 import express, { Application } from 'express'
 import healthRoutes from './routes/healthCheck'
 import validateRoutes from './routes/validate'
-import swaggerUi  from 'swagger-ui-express';
-import swaggerDocument  from './swagger_output.json';
-import cors from 'cors'; 
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger_output.json'
+import cors from 'cors'
+import path from 'path'
 const createServer = (): express.Application => {
   const app: Application = express()
+  const allowedOrigins = ['https://resolved-relieved-snake.ngrok-free.app', 'http://localhost:3008']
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin) return callback(null, true)
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+          return callback(new Error(msg), false)
+        }
+        return callback(null, true)
+      },
+    }),
+  )
   // Body parsing Middleware
   app.use(express.json({ limit: '50mb' }))
 
+  app.use(express.static(path.join(__dirname, 'ui')))
+
+  // For all other routes, serve index.html (React Router support)
+  app.get('/ui', (req, res) => {
+    req
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'))
+  })
+
   app.use('/', healthRoutes)
   app.use('/api', validateRoutes)
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   return app
 }
 
